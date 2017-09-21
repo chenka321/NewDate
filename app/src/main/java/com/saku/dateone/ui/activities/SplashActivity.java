a17e1b92e84ac13a51014f75b66ca01cec633fda
chen_card <chenka321@gmail.com>
2017-09-21 21:01:52 +0800

    测试

--- app/src/main/java/com/saku/dateone/ui/activities/SplashActivity.java
+++ app/src/main/java/com/saku/dateone/ui/activities/SplashActivity.java
@@ -38,11 +38,6 @@ public class SplashActivity extends BaseActivity implements LocationListener {
     private Location mLocation;
     private FrameLayout mFrameLayout;
 
-
-
-
-
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
